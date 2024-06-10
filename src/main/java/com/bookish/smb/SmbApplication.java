d9a5a6ed4f5bd50975d960d6e7965d4fbe26ba94
Kharka <adewoyindaniel6@gmail.com>
2024-06-10 20:43:21 +0100

    Initial commit

---
+++ src/main/java/com/bookish/smb/SmbApplication.java
@@ -0,0 +1,13 @@
+package com.bookish.smb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookish/smb/SmbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookish.smb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
