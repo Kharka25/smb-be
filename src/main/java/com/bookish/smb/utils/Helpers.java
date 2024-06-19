f07be43b11c5ce12bafe76d14795f38837ac841d
Kharka <adewoyindaniel6@gmail.com>
2024-06-19 10:48:31 +0100

    otp minimum length check

--- src/main/java/com/bookish/smb/utils/Helpers.java
+++ src/main/java/com/bookish/smb/utils/Helpers.java
@@ -7,13 +7,13 @@ import java.util.UUID;
 import java.util.regex.Pattern;
 
 @Service
-public class Helpers {
+public abstract class Helpers {
 
     public static String generateOtp(int otpLength) {
         StringBuilder otp = new StringBuilder();
         Random random = new Random();
 
-        if(otpLength <= 3) return new IllegalArgumentException("").getMessage();
+        if(otpLength <= 3) throw new IllegalArgumentException("OTP length is too short");
 
         for(int i = 1; i <= otpLength; i++) {
             int randomNumber = random.nextInt(1, 9);
